f40d1f25ac7d97a18209e30ee1419e1a9dfae73b
João Martins <martins.j.neto@gmail.com>
2018-06-03 17:04:52 -0300

    feat: Add Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import * as BooksAPI from './BooksAPI'
+import Header from './components/header';
 import './App.css';
 
 const BooksApp = () => (
   <div className="app">
     <div className="list-books">
-      <h1>Hello World</h1>
+      <Header title="MyReads" />
     </div>
   </div>
 );
